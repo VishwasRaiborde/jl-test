657be43e7af58b7bd865141738119539df562629
VishwasRaiborde <42356895+VishwasRaiborde@users.noreply.github.com>
2018-12-27 00:29:01 +0530

    git

--- pricing-engine/product-pricing-engine-endpoint/src/test/java/com/jl/product/service/ProductServiceTest.java
+++ pricing-engine/product-pricing-engine-endpoint/src/test/java/com/jl/product/service/ProductServiceTest.java
@@ -43,7 +43,7 @@ public class ProductServiceTest {
 		List<ProductVO> decoreatedProductVOs = productService.getProducedsWithWithFilter(filter);
 		for (ProductVO productVO : decoreatedProductVOs) {
 			System.out.println(productVO.toString());
-		}
+		} 
 	}
 
 }
